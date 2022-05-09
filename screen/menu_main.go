c331ea776f38351aa005a09e21e856bb42bf9501
Pavel Zaichenkov <zaichenkov@gmail.com>
2022-05-09 23:12:02 +0200

    experiment with fonts

--- screen/menu_main.go
+++ screen/menu_main.go
@@ -4,6 +4,7 @@ import (
 	"eklase/state"
 
 	"gioui.org/layout"
+	"gioui.org/text"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
@@ -16,10 +17,17 @@ func MainMenu(th *material.Theme, state *state.State) Screen {
 		quit widget.Clickable
 	)
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
+		matAddBut := material.Button(th, &add, "Add student")
+		matAddBut.Font = text.Font{Variant: "Mono", Weight: text.Bold}
+		matListBut := material.Button(th, &list, "List students")
+		matListBut.Font = text.Font{Variant: "Mono", Weight: text.Bold, Style: text.Italic}
+		matQuitBut := material.Button(th, &quit, "Quit")
+		matQuitBut.Font = text.Font{Variant: "Smallcaps", Style: text.Italic}
+
 		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-			layout.Rigid(rowInset(material.Button(th, &add, "Add student").Layout)),
-			layout.Rigid(rowInset(material.Button(th, &list, "List students").Layout)),
-			layout.Rigid(rowInset(material.Button(th, &quit, "Quit").Layout)),
+			layout.Rigid(rowInset(matAddBut.Layout)),
+			layout.Rigid(rowInset(matListBut.Layout)),
+			layout.Rigid(rowInset(matQuitBut.Layout)),
 		)
 		if add.Clicked() {
 			return AddStudent(th, state), d
